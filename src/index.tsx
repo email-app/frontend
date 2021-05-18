c1f09e9b05dbffeb7bc7ef89f394df380d1ee8f8
Alexander Baron <schwefelhexaflurid@gmail.com>
2021-05-18 12:12:27 +0200

    Setup

---
+++ src/index.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+ReactDOM.render(
+  <div className="h-full flex items-center justify-center">
+    <p className="text-red-500 text-8xl">Hello React</p>
+  </div>,
+  document.getElementById("app")
+);
