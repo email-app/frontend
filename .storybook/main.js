25f4a3095e7250c67a0543a7c5dd3e1140113a08
Alexander Baron <schwefelhexaflurid@gmail.com>
2021-05-21 21:23:03 +0200

    fix (storybook): Use webpack 8

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,10 +1,16 @@
 module.exports = {
-  "stories": [
-    "../src/**/*.stories.mdx",
-    "../src/**/*.stories.@(js|jsx|ts|tsx)"
+  stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    {
+      name: '@storybook/addon-postcss',
+      options: {
+        // Use postcss 8
+        postcssLoaderOptions: {
+          implementation: require('postcss'),
+        },
+      },
+    },
   ],
-  "addons": [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials"
-  ]
-}
+};
