19141f80205b97906534723ba237e5de7ac0c38d
Alexander Baron <schwefelhexaflurid@gmail.com>
2021-05-21 20:56:09 +0200

    feat (Navbar): Current element styling

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 import {
   CloudUploadIcon,
   FilterIcon,
@@ -12,15 +12,17 @@ import {
 import classNames from 'src/util/classNames';
 
 const navigation = [
-  { name: 'Inbox', href: '/emails', icon: InboxIcon, current: true },
-  { name: 'Accounts', href: '/accounts', icon: UsersIcon, current: false },
-  { name: 'Spam', href: '/spam', icon: FlagIcon, current: false },
-  { name: 'Sent', href: '/sent', icon: CloudUploadIcon, current: false },
-  { name: 'Filters', href: '/filters', icon: FilterIcon, current: false },
-  { name: 'Trash', href: '/trash', icon: TrashIcon, current: false },
+  { name: 'Inbox', href: '/emails', icon: InboxIcon },
+  { name: 'Accounts', href: '/accounts', icon: UsersIcon },
+  { name: 'Spam', href: '/spam', icon: FlagIcon },
+  { name: 'Sent', href: '/sent', icon: CloudUploadIcon },
+  { name: 'Filters', href: '/filters', icon: FilterIcon },
+  { name: 'Trash', href: '/trash', icon: TrashIcon },
 ];
 
 const Navbar: React.FC = () => {
+  const { pathname } = useLocation();
+
   return (
     <div className="flex flex-col flex-1 h-0 bg-gray-100 border-r border-gray-200">
       <div className="flex flex-col flex-1 pt-5 pb-4 overflow-y-auto">
@@ -33,29 +35,31 @@ const Navbar: React.FC = () => {
         </div>
         <nav className="flex-1 mt-5" aria-label="Sidebar">
           <div className="px-2 space-y-1">
-            {navigation.map((item) => (
-              <Link
-                key={item.name}
-                to={item.href}
-                className={classNames(
-                  item.current
-                    ? 'bg-gray-200 text-gray-900'
-                    : 'text-gray-600 hover:bg-gray-50 hover:text-gray-900',
-                  'group flex items-center px-2 py-2 text-base lg:text-sm font-medium rounded-md'
-                )}
-              >
-                <item.icon
+            {navigation
+              .map((i) => ({ ...i, current: pathname.startsWith(i.href) }))
+              .map((item) => (
+                <Link
+                  key={item.name}
+                  to={item.href}
                   className={classNames(
                     item.current
-                      ? 'text-gray-500'
-                      : 'text-gray-400 group-hover:text-gray-500',
-                    'mr-4 lg:mr-3 h-6 w-6'
+                      ? 'bg-gray-200 text-gray-900'
+                      : 'text-gray-600 hover:bg-gray-50 hover:text-gray-900',
+                    'group flex items-center px-2 py-2 text-base lg:text-sm font-medium rounded-md'
                   )}
-                  aria-hidden="true"
-                />
-                {item.name}
-              </Link>
-            ))}
+                >
+                  <item.icon
+                    className={classNames(
+                      item.current
+                        ? 'text-gray-500'
+                        : 'text-gray-400 group-hover:text-gray-500',
+                      'mr-4 lg:mr-3 h-6 w-6'
+                    )}
+                    aria-hidden="true"
+                  />
+                  {item.name}
+                </Link>
+              ))}
           </div>
         </nav>
       </div>
