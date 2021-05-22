568f1559966584a1b94ef14c12733ccc9870ad20
Alexander Baron <schwefelhexaflurid@gmail.com>
2021-05-22 12:08:26 +0200

    fix (EmailView): Tage markup from props

--- src/components/controllers/EmailViewController.tsx
+++ src/components/controllers/EmailViewController.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
+
+import email from 'src/data/email.html.txt';
 import EmailView from '../EmailView';
 
-const EmailViewController: React.FC = () => <EmailView />;
+const EmailViewController: React.FC = () => <EmailView raw={email} />;
 export default EmailViewController;
