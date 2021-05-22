134a6b172a6d995d2316ddf9ccacf6450c81ff9b
Alexander Baron <schwefelhexaflurid@gmail.com>
2021-05-22 12:15:36 +0200

    Render email from API

--- src/components/controllers/EmailViewController.tsx
+++ src/components/controllers/EmailViewController.tsx
@@ -1,7 +1,27 @@
 import React from 'react';
+import { gql } from '@apollo/client';
 
-import email from 'src/data/email.html.txt';
+import { useEmailViewQuery } from 'src/generated/graphql';
 import EmailView from '../EmailView';
 
-const EmailViewController: React.FC = () => <EmailView raw={email} />;
+const EMAIL_VIEW_QUERY = gql`
+  query EmailView($id: String!) {
+    email(id: $id) {
+      id
+      content
+    }
+  }
+`;
+
+export interface EmailViewControllerProps {
+  id: string;
+}
+const EmailViewController: React.FC<EmailViewControllerProps> = ({ id }) => {
+  const { data, loading, error } = useEmailViewQuery({ variables: { id } });
+
+  if (data) return <EmailView raw={data.email.content} />;
+  if (error) return <p>Error!</p>;
+  if (loading) return <p>Loading...</p>;
+  return null;
+};
 export default EmailViewController;
--- src/pages/EmailDetailPage.tsx
+++ src/pages/EmailDetailPage.tsx
@@ -1,19 +1,24 @@
 import React from 'react';
+import { useParams } from 'react-router';
 
 import EmailListController from 'src/components/controllers/EmailListController';
 import EmailViewController from 'src/components/controllers/EmailViewController';
 import MainAsideLayout from 'src/layout/MainAsideLayout';
 
-const EmailDetailPage: React.FC = () => (
-  <MainAsideLayout>
-    <MainAsideLayout.Main>
-      <div className="max-w-3xl mx-auto">
-        <EmailViewController />
-      </div>
-    </MainAsideLayout.Main>
-    <MainAsideLayout.Aside>
-      <EmailListController />
-    </MainAsideLayout.Aside>
-  </MainAsideLayout>
-);
+const EmailDetailPage: React.FC = () => {
+  const { id } = useParams() as { id: string };
+
+  return (
+    <MainAsideLayout>
+      <MainAsideLayout.Main>
+        <div className="max-w-3xl mx-auto">
+          <EmailViewController id={id} />
+        </div>
+      </MainAsideLayout.Main>
+      <MainAsideLayout.Aside>
+        <EmailListController />
+      </MainAsideLayout.Aside>
+    </MainAsideLayout>
+  );
+};
 export default EmailDetailPage;
