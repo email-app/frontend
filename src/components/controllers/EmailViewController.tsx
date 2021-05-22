f1c1d3aa6038a57afe8c4ddf90c0cd5a58a22104
Alexander Baron <schwefelhexaflurid@gmail.com>
2021-05-22 12:07:12 +0200

    Mock EmailViewController

---
+++ src/components/controllers/EmailViewController.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+import EmailView from '../EmailView';
+
+const EmailViewController: React.FC = () => <EmailView />;
+export default EmailViewController;
--- src/pages/EmailDetailPage.tsx
+++ src/pages/EmailDetailPage.tsx
@@ -1,14 +1,14 @@
 import React from 'react';
 
 import EmailListController from 'src/components/controllers/EmailListController';
-import EmailView from 'src/components/EmailView';
+import EmailViewController from 'src/components/controllers/EmailViewController';
 import MainAsideLayout from 'src/layout/MainAsideLayout';
 
 const EmailDetailPage: React.FC = () => (
   <MainAsideLayout>
     <MainAsideLayout.Main>
       <div className="max-w-3xl mx-auto">
-        <EmailView />
+        <EmailViewController />
       </div>
     </MainAsideLayout.Main>
     <MainAsideLayout.Aside>
