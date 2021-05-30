fcfdc2145d9424f50bc5e59b7dc4d57f3500003d
Alexander Baron <schwefelhexaflurid@gmail.com>
2021-05-30 22:44:18 +0200

    Revert "Add mock schema"
    This reverts commit 4099fedc282489301a1cc234448089fdcd75e19f.

--- src/mirage.ts
+++ src/mirage.ts
@@ -1,8 +1,6 @@
 import { createServer } from 'miragejs';
 import { createGraphQLHandler } from '@miragejs/graphql';
 
-import schema from './schema.graphql';
-
 createServer({
   routes() {
     this.post('/graphql', createGraphQLHandler('', this.schema));
