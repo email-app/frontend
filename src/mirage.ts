4099fedc282489301a1cc234448089fdcd75e19f
Alexander Baron <schwefelhexaflurid@gmail.com>
2021-05-30 22:44:18 +0200

    Add mock schema

--- src/mirage.ts
+++ src/mirage.ts
@@ -1,6 +1,8 @@
 import { createServer } from 'miragejs';
 import { createGraphQLHandler } from '@miragejs/graphql';
 
+import schema from './schema.graphql';
+
 createServer({
   routes() {
     this.post('/graphql', createGraphQLHandler('', this.schema));
