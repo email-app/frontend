2f01a60aa24c80b3b40551bfdd349d5dee7747a6
Alexander Baron <schwefelhexaflurid@gmail.com>
2021-05-30 23:32:06 +0200

    fix: Run apollo setup after mirage

--- src/graphql/client.ts
+++ src/graphql/client.ts
@@ -1,7 +1,7 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client';
 
-const client = new ApolloClient({
-  uri: (import.meta as any).env.SNOWPACK_PUBLIC_GRAPHQL_URL,
-  cache: new InMemoryCache(),
-});
-export default client;
+export const createClient = () =>
+  new ApolloClient({
+    uri: (import.meta as any).env.SNOWPACK_PUBLIC_GRAPHQL_URL,
+    cache: new InMemoryCache(),
+  });
--- src/index.tsx
+++ src/index.tsx
@@ -3,8 +3,6 @@ import ReactDOM from 'react-dom';
 
 import App from './app';
 
-import './mirage'; // Import for side-effect
-
 ReactDOM.render(<App />, document.getElementById('app'));
 
 if ((import.meta as any).hot) {
